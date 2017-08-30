65a64069e98908d4745624dea2678d8a0feb90d4
James Kominick <james.kominick@gmail.com>
2017-08-29 21:13:40 -0400

    fix ycm config
    - Add boost include/libs locations

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,4 +1,11 @@
 # Required/used by YouCompleteMe for cxx files
+import os
+
+BOOST_ROOT = os.environ.get("BOOST_ROOT")
+BOOST_INCLUDE = BOOST_LIB = ''
+if BOOST_ROOT:
+    BOOST_INCLUDE = '-I' + BOOST_ROOT
+    BOOST_LIB = '-L' + os.path.join(BOOST_ROOT, 'stage', 'lib')
 
 def FlagsForFile(filename, **kwargs):
     return {
@@ -10,6 +17,8 @@ def FlagsForFile(filename, **kwargs):
             '-stdlib=libc++',
             '-x',
             'c++',
+            BOOST_INCLUDE,
+            BOOST_LIB,
         ],
     }
 
