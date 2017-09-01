c44c2427dabc9683a8c5cd7e3c9d1ff4fad6acd8
James Kominick <james.kominick@gmail.com>
2017-08-31 23:35:36 -0400

    update ycm config

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,24 +1,73 @@
-# Required/used by YouCompleteMe for cxx files
-import os
+##~ YouCompleteMe cxx config
 
+import os
+import json
+
+
+# Look for boost
 BOOST_ROOT = os.environ.get("BOOST_ROOT")
 BOOST_INCLUDE = BOOST_LIB = ''
 if BOOST_ROOT:
     BOOST_INCLUDE = '-I' + BOOST_ROOT
     BOOST_LIB = '-L' + os.path.join(BOOST_ROOT, 'stage', 'lib')
 
+# Set default flags
+DEFAULT_FLAGS = [
+    '-Wall',
+    '-Wall',
+    '-Wextra',
+    '-Werror',
+    '-Warray-bounds',
+    '-Wpedantic',
+    '-std=c++11',
+    '-stdlib=libc++',
+    '-x',
+    'c++',
+    '-I/usr/include',
+    '-I/usr/lib',
+    '-I/usr/local/include',
+    '-I/usr/local/lib',
+    '-Iinclude/',
+    '-Llibs/',
+    BOOST_INCLUDE,
+    BOOST_LIB,
+]
+
+
+def _find_cmake_dir(dir_):
+    """Walk up the fs, looking for a dir containing a CMakeLists.txt file"""
+    if os.path.isfile(os.path.join(dir_, "CMakeLists.txt")):
+        return dir_
+
+    parent = os.path.dirname(dir_)
+    if parent == dir_:
+        return
+
+    return _find_cmake_dir(parent)
+
+
+def _load_cmake_compile_flags(dir_):
+    """Look for cmake output file and return any extra compile-commands found"""
+    compile_commands_file = os.path.join(dir_, "compile_commands.json")
+    if not os.path.isfile(compile_commands_file):
+        return
+
+    with open(compile_commands_file, 'r') as f:
+        try:
+            compile_commands = json.load(f)[0]['command']
+            return [f for f in compile_commands.split(' ') if f]
+        except:
+            return
+
+
 def FlagsForFile(filename, **kwargs):
+    cmake_dir = _find_cmake_dir(os.path.dirname(filename))
+    if cmake_dir:
+        cmake_flags = _load_cmake_compile_flags(cmake_dir)
+        if cmake_flags:
+            DEFAULT_FLAGS.extend(cmake_flags)
+
     return {
-        'flags': [
-            '-Wall',
-            '-Wextra',
-            '-Werror',
-            '-std=c++11',
-            '-stdlib=libc++',
-            '-x',
-            'c++',
-            BOOST_INCLUDE,
-            BOOST_LIB,
-        ],
+        'flags': DEFAULT_FLAGS,
     }
 
