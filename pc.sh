1c916fbae22fd33d74caa5788ca40674bee5a464
James Kominick <james@kominick.com>
2021-06-11 02:27:31 +0000

    use upaste encryption key if it's set

--- pc.sh
+++ pc.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -e
+
+function post() {
+    infile="@-"
+    if [[ ! -z "$1" ]]; then
+        infile="@$1"
+    fi
+
+    if [[ -z "$UPASTE_ENCRYPTION_KEY" ]]; then
+        curl $UPASTE_PASTEROOT -s -d $infile
+    else
+        curl $UPASTE_PASTEROOT -s -d $infile -H "x-upaste-encryption-key: $UPASTE_ENCRYPTION_KEY"
+    fi
+}
+
 if [ -z "$1" ]; then
-    curl $UPASTE_PASTEROOT -s -d @- | jq -r .key | echo "$UPASTE_READROOT_RAW/$(cat -)"
+    post | jq -r .key | echo "$UPASTE_READROOT_RAW/$(cat -)"
 else
-    curl $UPASTE_PASTEROOT -s -d @$1 | jq -r .key | echo "$UPASTE_READROOT_RAW/$(cat -)"
+    post $1 | jq -r .key | echo "$UPASTE_READROOT_RAW/$(cat -)"
 fi
--- pp.sh
+++ pp.sh
@@ -4,7 +4,11 @@ if [[ -z "$1" ]]; then
     $0 "$(head -n 1)"
 else
     if [[ $1 =~ ^http.* ]]; then
-        curl $1 -s
+        if [[ -z "$UPASTE_ENCRYPTION_KEY" ]]; then
+            curl $1 -s
+        else
+            curl $1 -s -H "x-upaste-encryption-key: $UPASTE_ENCRYPTION_KEY"
+        fi
     else
         $0 $UPASTE_READROOT_RAW/$1
     fi
